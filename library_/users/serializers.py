f7189e59153d39caac5900f8243bfb3088c8a095
Станислав Викторович <nparanoid@gmail.com>
2021-05-16 15:24:50 +0300

    160521_
    160521_

--- library_/users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-
-from .models import Users
-
-
-class UsersModelSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = Users
-        fields = '__all__'
